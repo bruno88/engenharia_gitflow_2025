59bd548a37f2c7a88ebb05cf51d8cb07dabab6c6
kaikBeninca <kaikbeninca06@gmail.com>
2025-12-16 17:34:54 -0300

    Criação da funcionalidade de filtrar os carros por ano (ordens crescente e decrescente) e por ordem alfabética

--- models/veiculos/ArmazenamentoVeiculo.js
+++ models/veiculos/ArmazenamentoVeiculo.js
@@ -25,4 +25,46 @@ export default class ArmazenamentoVeiculo {
     static listarVeiculos() {
         return [...this.veiculos];
     }
+
+    static ordenarPorAno(veiculos, ordem) {
+        if (!ordem)
+            return veiculos;
+
+        return [...veiculos].sort((a, b) => {
+            switch (ordem) {
+                case "crescente":
+                    return a.ano - b.ano;
+                case "decrescente":
+                    return b.ano - a.ano;
+            }
+        });
+    }
+
+    static ordenarPorNome(veiculos, ordem) {
+        if (!ordem)
+            return veiculos;
+
+        return [...veiculos].sort((a, b) => {
+            switch (ordem) {
+                case "crescente":
+                    return a.modelo.localeCompare(b.modelo);
+                case "decrescente":
+                    return b.modelo.localeCompare(a.modelo);
+            }
+        });
+    }
+
+    static filtrarVeiculos(veiculos, filtroAno = null, filtroNome = null) {
+        let resultado = [...veiculos];
+
+        if (filtroAno) {
+            resultado = this.ordenarPorAno(resultado, filtroAno);
+        }
+
+        if (filtroNome) {
+            resultado = this.ordenarPorNome(resultado, filtroNome);
+        }
+
+        return resultado;
+    }
 }
